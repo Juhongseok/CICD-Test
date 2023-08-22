ed4cd7ec109d19f09d12160698dcc97741233671
JHS <wnghdtjr129@naver.com>
2023-08-22 19:29:45 +0900

    first commit

---
+++ src/main/java/com/example/springcicd/SpringCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCicdApplication.class, args);
+    }
+
+}

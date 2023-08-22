ef1507d608a21cea05bbf367972881ea658566b6
JHS <wnghdtjr129@naver.com>
2023-08-22 19:13:09 +0900

    first commit

---
+++ src/test/java/com/jhs/springcicd/SpringCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhs.springcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

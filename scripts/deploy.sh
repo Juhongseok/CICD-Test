82d60b93ee76e335e7c1004f5b2564cc6f28244e
JHS <wnghdtjr129@naver.com>
2023-08-25 16:21:34 +0900

    deploy 설정 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ubuntu/code
+PROJECT_NAME=spring-cicd
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+cd $REPOSITORY
+
+CURRENT_PID=$(pgrep -f $PROJECT_NAME.*.jar)
+
+if [ -z ""$CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다"
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy - $JAR_PATH"
+sudo nohup java -jar $JAR_PATH &
